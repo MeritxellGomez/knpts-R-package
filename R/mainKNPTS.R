17c12ceebac78c403f89cb26d90976cb85c04d85
mgomez <mgomez@tekniker.es>
2021-01-13 15:31:05 +0100

    main script and predictKNPTS function created

---
+++ R/mainKNPTS.R
@@ -0,0 +1,18 @@
+#' Make predictions using KNPTS (K-Nearest Patterns in Time Series)
+#'
+#' @param ts
+#' @param h
+#' @param n
+#'
+#' @return model
+#' @export
+#'
+#' @examples
+predictKNPTS <- function(ts, h, n){
+
+
+  model <- h+n
+  return(model)
+
+
+}
