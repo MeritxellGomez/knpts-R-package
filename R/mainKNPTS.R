f42ef6bd9aa10906c4125b3fdff271f300aa3b13
mgomez <mgomez@tekniker.es>
2021-04-28 10:10:56 +0200

    added two strategies in predict (mimo and recursive)

--- R/mainKNPTS.R
+++ R/mainKNPTS.R
@@ -1,14 +1,14 @@
 #' Make predictions using KNPTS (K-Nearest Patterns in Time Series)
 #'
-#' @param ts
-#' @param h
+#' @param ts univariate time series
+#' @param h number of points in the horizon series
 #' @param n
 #'
 #' @return model
 #' @export
 #'
 #' @examples
-predictKNPTS <- function(ts, h = 5, n = 3, kmax = 10, dist = 'Euclidean', pond = FALSE){
+predictKNPTS <- function(ts, h = 5, n = 3, kmax = 10, dist = 'Euclidean', pond = FALSE, strategy = 'mimo'){
 
   #coger los h ultimos valores de la serie
   horizon <- horizon(ts, h)
@@ -16,14 +16,18 @@ predictKNPTS <- function(ts, h = 5, n = 3, kmax = 10, dist = 'Euclidean', pond =
   #CV para escoger la mejor k. De momento pongo k=3 hasta que esté hecha la funcion de crossvalidation
   kopt <- 3
 
-  #calcular las distancias
-  distances <- distance_knn(ts, horizon, n, dist)
-
-  #y coger las k mas pequeñas con sus n siguientes valores
-  kneighbors <- distances[1:kopt,]
-
-  #combinar los n siguientes valores y meter en un vector de predicciones
-  predictions <- predictknn(ts, kneighbors = kneighbors, n, pond)
+  if(strategy == 'mimo'){
+    #calcular las distancias
+    #y coger las k mas pequeñas con sus n siguientes valores
+    distances <- distance_knn(ts, horizon, n, dist)
+    kneighbors <- distances[1:kopt,]
+    #combinar los n siguientes valores y meter en un vector de predicciones
+    predictions <- predictknn_mimo(ts, kneighbors = kneighbors, n, pond)
+  }else if(strategy == 'recursive'){
+    predictions <- predictknn_recursive(ts, horizon = horizon, h = h, n = n, pond = pond, dist = dist, kopt = kopt)
+  }else{
+    stop('incorrect name for strategy argument')
+  }
 
   return(predictions)
 
