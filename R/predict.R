dbd5fa7d1e4d5c8a872f768e0009f80d5267122d
mgomez <mgomez@tekniker.es>
2021-04-28 10:11:16 +0200

    predict is renamed to predict_mimo and predict_recursive is added

--- R/predict.R
+++ R/predict.R
@@ -1,4 +1,4 @@
-predictknn <- function(ts, kneighbors, n, pond){
+predictknn_mimo <- function(ts, kneighbors, n, pond){
 
   #coger el segundo indice de kneighbors y copiar los npred siguientes
   nearest.neighbors <- kneighbors[['final.value']]
@@ -28,6 +28,44 @@ predictknn <- function(ts, kneighbors, n, pond){
     pred <- apply(m, 2, mean)
   }
   #devolver vector de n longitud
+
+  return(pred)
+
+}
+
+predictknn_recursive <- function(ts, horizon, h,  n, dist, kopt, pond){
+
+  pred <- c()
+
+  for(i in 1:n){
+
+    distances <- distance_knn(ts, horizon, n, dist)
+    kneighbors <- distances[1:kopt,]
+
+    pos_combination <- kneighbors$final.value + 1
+    values_combination <- ts[pos_combination]
+
+
+    if(kneighbors[['distances']]==0){pond==FALSE} #avoid dividing by 0
+
+    if(pond){
+      w <- (1/(kneighbors[['distances']])^2)
+
+      nw <- w/(sum(w))
+
+      aux_pred <- nw * m
+
+    }else{
+      aux_pred <- mean(values_combination)
+    }
+
+    pred <- c(pred, aux_pred)
+
+    ts <- c(ts, aux_pred)
+
+    horizon <- horizon(ts, h)
+
+  }
 
   return(pred)
 
